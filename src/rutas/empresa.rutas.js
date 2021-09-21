11b1b9f37e4994d587cded795fc95d5731b5d2e0
Jose Julian Salazar Melgar <josejuliansalazarmelgar@gmail.com>
2021-09-21 01:08:18 -0600

    Creacion de rutas de Empresa

--- src/rutas/empresa.rutas.js
+++ src/rutas/empresa.rutas.js
@@ -7,5 +7,11 @@ var md_authorization = require("../middlewares/authenticated");
 
 var api = express.Router();
 api.get('/ejemploEmpresas', empresa_controlador.ejemploEmpresa);
+api.post('/loginEmpresa', empresa_controlador.loginEmpresa);
+api.post('/agregarEmpresa', md_authorization.ensureAuth, empresa_controlador.agregarEmpresa);
+api.get('/obtenerEmpresas', md_authorization.ensureAuth, empresa_controlador.obtenerEmpresas);
+api.get('/obtenerEmpresaID/:idEmpresa', md_authorization.ensureAuth, empresa_controlador.obtenerEmpresaID);
+api.put('/editarEmpresa/:id', md_authorization.ensureAuth, empresa_controlador.editarEmpresa);
+api.delete('/eliminarEmpresa/:id', md_authorization.ensureAuth, empresa_controlador.eliminarEmpresa);
 
 module.exports = api;
