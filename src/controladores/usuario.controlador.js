c35532f368425f1172fc1011d7329b1f32e915c2
Jose Julian Salazar Melgar <josejuliansalazarmelgar@gmail.com>
2021-09-21 01:02:21 -0600

    Creacion de login admin y agregra empleados 21/09

--- src/controladores/usuario.controlador.js
+++ src/controladores/usuario.controlador.js
@@ -49,7 +49,82 @@ function Admin(req, res){
     })
 }
 
+
+function loginAdmin(req, res) {
+    var params = req.body;
+   
+    Usuario.findOne({ usuario: params.usuario }, (err, usuarioEncontrado) => {
+        if (err) return res.status(500).send({ mensaje: 'Error en la peticion' });
+
+        if (usuarioEncontrado) {
+            bcrypt.compare(params.password, usuarioEncontrado.password, (err, passVerificada) => {
+                if (passVerificada) {
+                    if (params.getToken === 'true') {
+                        return res.status(200).send({
+                            token: jwt.createToken(usuarioEncontrado)
+                        })
+                    } else {
+                        usuarioEncontrado.password = undefined;
+                        return res.status(200).send({ usuarioEncontrado });
+                    }
+                } else {
+                    return res.status(500).send({ mensaje: 'El usuario no se a podido identificar' });
+                }
+            })
+        } else {
+            return res.status(500).send({ mensaje: 'Error al buscar el usuario' });
+        }
+    })
+}
+
+
+function agregraEmpleado(req, res){
+    
+    var empleadoModel = new Usuario();
+    var params = req.body;
+
+    if(req.user.rol != 'empresa'){
+        return res.status(500).send({mensaje: 'Solo el rol de tipo empresa puede crear usuarios'});
+    }
+
+    if(params.nombre && params.puesto && params.departamento){
+        
+        empleadoModel.nombre = params.nombre;
+        empleadoModel.rol = 'empleado',
+        empleadoModel.puesto = params.puesto;
+        empleadoModel.departamento = params.departamento;
+        empleadoModel.empleadoEmpresa = req.user.nombre;
+
+
+        Usuario.find({ 
+            $or: [
+                { nombre: empleadoModel.nombre},    
+            ]
+        }).exec((err, empleadoEncontrado)=>{
+            if(err) return res.status(500).send({mensaje: 'Error en la peticion de crear el Empleado'});
+
+            if(empleadoEncontrado && empleadoEncontrado.length >= 1){
+                return res.status(500).send({mensaje: 'El empleado ya se encuentra creado dentro del sistema'});
+            } else{
+                
+                empleadoModel.save((err, empleadoGuardado)=>{
+                    if(err) return res.status(500).send({mensaje: 'Error en la peticion de guardar el empleado'});
+
+                    if(empleadoGuardado){
+                        res.status(200).send({ empleadoGuardado })
+                    } else{
+                        return res.status(404).send({ mensaje: 'No se ha podido registrar al empleado'});
+                    }
+                })
+            }
+        })
+    }
+}
+
 module.exports = {
     ejemplo,
-    Admin
+    Admin,
+    loginAdmin,
+    agregraEmpleado
+
 }
