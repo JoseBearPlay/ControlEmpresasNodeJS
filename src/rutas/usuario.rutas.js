09e9fbd220941528e51312222dd0460befd7f1e0
Jose Julian Salazar Melgar <josejuliansalazarmelgar@gmail.com>
2021-09-21 01:10:11 -0600

    Creacion de nuevas rutas en las rutas de Empleados

--- src/rutas/usuario.rutas.js
+++ src/rutas/usuario.rutas.js
@@ -8,5 +8,8 @@ var md_authorization = require("../middlewares/authenticated");
 var api = express.Router();
 api.get('/ejemploEmpleados', usuario_controlador.ejemplo);
 api.get('/admin', usuario_controlador.Admin);
+api.post('/loginAdmin', usuario_controlador.loginAdmin);
+api.post('/agregraEmpleado', md_authorization.ensureAuth, usuario_controlador.agregraEmpleado);
+
 
 module.exports = api;
