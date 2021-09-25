cc2c2ab93144fbaac70df157c77603019eb4982b
Jsalazar <jsalazar-2019070@kinal.edu.gt>
2021-09-25 10:06:30 -0600

    Rutas ordenar mayor a menor viceversa productos

--- src/rutas/producto.rutas.js
+++ src/rutas/producto.rutas.js
@@ -8,5 +8,7 @@ var md_authorization = require("../middlewares/authenticated");
 var api = express.Router();
 api.get('/ejemploProducto', producto_controlador.ejemploProducto);
 api.post('/agregarProducto', md_authorization.ensureAuth, producto_controlador.agregarProducto);
+api.get('/ordenarProductosAscendente', md_authorization.ensureAuth, producto_controlador.ordenarProductosAscendente);
+api.get('/ordenarProductosDescendente', md_authorization.ensureAuth, producto_controlador.ordenarProductosDescendente);
 
 module.exports = api;
