293f5a3ae6f01499e043abef93cb190b2d642113
Jsalazar <jsalazar-2019070@kinal.edu.gt>
2021-09-25 10:45:30 -0600

    Función de obtener productos, obtener productos por nombre y obtener productos por proveedor

--- src/controladores/producto.controlador.js
+++ src/controladores/producto.controlador.js
@@ -19,7 +19,7 @@ function agregarProducto(req, res){
         productoModel.nombre = params.nombre;
         productoModel.proveedor = params.proveedor;
         productoModel.stock = params.stock;
-        productoModel.vendido = params.vendido;
+        productoModel.vendido = 0;
         productoModel.productoEmpresa = req.user.nombre
 
 
@@ -79,9 +79,86 @@ function ordenarProductosDescendente(req, res){
 }
 
 
+function obtenerProductos(req, res){
+
+    var params = req.body;
+
+    if(req.user.rol != 'empresa'){
+        return res.status(500).send({mensaje: 'Solo el rol tipo empresa puede ver los productos agregados a la empresa'});
+    }
+
+    if(params.nombre === req.user.nombre){
+
+        Producto.find({ 
+
+            productoEmpresa: req.user.nombre
+        
+        }).exec((err, productosReconocidos)=>{
+            if(err) return res.status(500).send({mensaje: 'Error en la peticion'});
+
+            if(!productosReconocidos) return res.status(500).send({mensaje: 'No se han podido obtener los productos o no posee productos registrados'})
+    
+            return res.status(200).send({ productosReconocidos });
+        })
+    } else{
+        return res.status(500).send({ mensaje: 'Solo puedes ver los productos de tu propia empresa'});
+    }
+}
+
+
+function obtenerProductoNombre(req, res){
+
+    var params = req.body;
+
+    if(req.user.rol != 'empresa'){
+        return res.status(500).send({mensaje: 'Solo el rol tipo empresa puede buscar productos en la empresa'});
+    }
+
+    Producto.findOne({ 
+
+        nombre: params.nombre
+
+    }).exec((err, productoReconocido)=>{
+        if(err) return res.status(500).send({mensaje: 'Error en la peticion'});
+
+        if(!productoReconocido) return res.status(500).send({mensaje: 'No existe el nombre del producto'});
+
+        if(productoReconocido.productoEmpresa != req.user.nombre) return res.status(500).send({mensaje: 'El producto no existe en la empresa'});
+
+        return res.status(200).send({ productoReconocido})
+    })
+}
+
+function obtenerProductoProveedor(req, res){
+
+    var params = req.body;
+
+    if(req.user.rol != 'empresa'){
+        return res.status(500).send({mensaje: 'Solo el rol tipo empresa puede buscar productos en la empresa'});
+    }
+
+    Producto.findOne({ 
+
+        proveedor: params.proveedor
+
+    }).exec((err, productoReconocido)=>{
+        if(err) return res.status(500).send({mensaje: 'Error en la peticion'});
+
+        if(!productoReconocido) return res.status(500).send({mensaje: 'No existe el proveedor del producto'});
+
+        if(productoReconocido.productoEmpresa != req.user.nombre) return res.status(500).send({mensaje: 'El proveedor no existe en la empresa'});
+
+        return res.status(200).send({ productoReconocido})
+    })
+}
+
+
 module.exports = {
     ejemploProducto,
     agregarProducto,
     ordenarProductosAscendente,
-    ordenarProductosDescendente
+    ordenarProductosDescendente,
+    obtenerProductos,
+    obtenerProductoNombre,
+    obtenerProductoProveedor
 }
