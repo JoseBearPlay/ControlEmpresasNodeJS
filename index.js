3c4fe5ff6e1239fa6e549526f40f1660a76a5349
Jsalazar <jsalazar-2019070@kinal.edu.gt>
2021-09-26 14:09:40 -0600

    Cambios en Index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var Usuario = require("./src/controladores/usuario.controlador");
 mongoose.Promise = global.Promise
 mongoose.connect(/*'mongodb+srv://Admin:123añs@empresas.fqhkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'*/ 'mongodb://localhost:27017/dbControlEmpresas', {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
     
-    Usuario.Admin();
+    Usuario.usuarioAdmin();
 
     console.log('Se encuentra conectado a la base de datos');
 
