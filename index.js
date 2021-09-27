c20463e44ef0bd48dae28228fb132165e3c32b97
Jsalazar <jsalazar-2019070@kinal.edu.gt>
2021-09-26 20:26:40 -0600

    Cambios Index

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = require("./app");
 var Usuario = require("./src/controladores/usuario.controlador");
 
 mongoose.Promise = global.Promise
-mongoose.connect(/*'mongodb+srv://Admin:123añs@empresas.fqhkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'*/ 'mongodb://localhost:27017/dbControlEmpresas', {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+mongoose.connect(  'mongodb+srv://Admin:123añs@empresas.fqhkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
     
     Usuario.usuarioAdmin();
 
     console.log('Se encuentra conectado a la base de datos');
 
-    app.listen(3000, function (){
+    app.listen(process.env.PORT || 3000, function (){
         console.log("Servidor corriendo en el puerto 3000");
     })
 }).catch(err => console.log(err));
